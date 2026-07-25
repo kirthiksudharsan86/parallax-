d3565e400afaed3c4f015475162d9bbf98ef5782
Kirthik Sudharsan <kirthiksudharsan.m2025@vitstudent.ac.in>
2026-07-25 20:58:28 +0530

    recent push

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
@@ -8,6 +9,9 @@ urlpatterns = [
     path('profile/complete/', views.profile_complete, name='profile_complete'),
     path('register/', views.register_team, name='register_team'),
     path('dashboard/', views.participant_dashboard, name='participant_dashboard'),
+    path('registration/', views.registration_index, name='registration_index'),
+    path('registration/team/', views.registration_leader, name='registration_leader'),
+    path('registration/payment/', views.registration_payment, name='registration_payment'),
     path('admin-panel/', views.admin_panel, name='admin_panel'),
     path('admin-panel/teams/', views.admin_teams, name='admin_teams'),
     path('admin-panel/marks/', views.admin_marks, name='admin_marks'),
